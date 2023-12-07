7096f732ad864732f6b05a843f603fa8704ae2e0
Sam4377 <tenz4377@gmail.com>
2023-12-07 14:31:51 -0500

    final edit

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,21 +1,46 @@
 import { useState } from "react"
 
-const [username, setUsername] = useState("")
-const [password, setPassword] = useState("")
-const [error, setError] = useState(null)
+
 
 export default function SignUpForm() {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState(null)
+
     async function handleSubmit(event) {
-        event.preventDefault()
-        console.log("yup")
+        event.preventDefault();
+        try {
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, password }),
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.message || "Sign up failed"); // Throw error with message from response data or default message
+            }
+            
+            console.log("Sign up successful:", data);
+            setUsername("");
+            setPassword("");
+            setError(null);
+        } catch (error) {
+            setError(error.message || "Something went wrong. Please try again.");
+        }
     }
     return <div><h2> Sign up! </h2>
+    {error && <p>{error}</p>}
     <form onSubmit={handleSubmit}>
         <label>
             Username: <input value={username} onChange={(e) => setUsername(e.target.value)}/>
         </label>
         <label>
-            Password: <input value={password} onChange={(b) => setPassword(b.target.value)}/>
+            Password: <input value={password} onChange={(e) => setPassword(e.target.value)}/>
         </label>
         <button> Submit</button>
     </form>
